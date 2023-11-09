04809440b0be906adcdb6f2c3c872bf6b66db05f
splash2009 <hrishikesh.galphade@splashgain.com>
2023-11-09 23:25:16 +0530

    updated for fdropdown radio, patchvalue,setvalue method. With default updation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,47 @@
-import { Component,ViewChild } from '@angular/core';
+import { Component,OnInit,ViewChild,AfterContentInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
+  selector: '[app-login]',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  @ViewChild('f') loginfom;
+export class LoginComponent  {
+  
+  
+  @ViewChild('f') loginfom ;
+
+  genders=['Male','Female','Transgender','abc','tyx']
+
+  emailDefaultValue:string="samdhan@f.com"
  //onSubmit(formElemnt:HTMLFormElement){
    // console.log(formElemnt);}
-
+  
   onSubmit(formElement:NgForm){
     console.log(formElement);
   }
 
   checkFormStatus(){
-    console.log(this.loginfom);
+    console.log(this.loginfom.value);
+  }
+
+  setDefaultValue(){
+    //Set Value update all the fileds/controls from the form. and you must provide all the form values.
+    /*this.loginfom.setValue({
+        usercredentials: {
+            "emailAddress": "samdhan@f.com",
+            "password": "Test@123"
+        },
+        role: "superadmin",
+        gender: "Male"
+    });*/
+
+    //In patch value method we can update only those fileds that we require and you can pass this filed to pathcVlaue function.
+    this.loginfom.form.patchValue({
+      usercredentials: {
+          "emailAddress": "samdhan@f.com",
+          "password": "Test@123"
+      }
+  });
   }
 }
